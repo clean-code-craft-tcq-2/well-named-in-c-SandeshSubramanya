77fd59179dba3ebc4f3ea0cfb081e1e1dd61ad76
SandeshSubramanya <91111862+SandeshSubramanya@users.noreply.github.com>
2022-01-11 23:59:34 +0530

    Update referenceManual.h

--- referenceManual.h
+++ referenceManual.h
@@ -7,13 +7,13 @@
 
 void vDisplayColorCodingMapping()
 {
-	printf("**********Reference Manual**********");
-	printf("MajorColor,MinorColor :: MajorColorCode, MinorColorCode\n");
+	printf("\n\t\t**********Reference Manual**********\n");
+	printf("\nMajorColor\tMinorColor \t:: \tMajorColorCode \tMinorColorCode\n");
 	for (int indexMajorColor = 0; indexMajorColor < numberOfMajorColors; ++indexMajorColor)
 	{
 		for (int indexMinorColor = 0; indexMinorColor < numberOfMinorColors; ++indexMinorColor)
 		{
-			printf("%s,%s :: %d,%d \n", MajorColorNames[indexMajorColor],MinorColorNames[indexMinorColor],indexMajorColor + 1, indexMinorColor + 1);
+			printf("%s \t\t%s \t\t:: \t%d \t%d \n", MajorColorNames[indexMajorColor],MinorColorNames[indexMinorColor],indexMajorColor + 1, indexMinorColor + 1);
 		}
 		
 		printf("\n\n");
