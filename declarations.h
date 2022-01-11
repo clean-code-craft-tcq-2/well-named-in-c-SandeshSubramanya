fc917837b40431a87ff0487ad34d1619f85f000c
SandeshSubramanya <91111862+SandeshSubramanya@users.noreply.github.com>
2022-01-11 23:01:33 +0530

    Introduce statics

--- declarations.h
+++ declarations.h
@@ -4,20 +4,20 @@
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
-const char* MajorColorNames[] = {
+static const char* MajorColorNames[] = {
     "White", "Red", "Black", "Yellow", "Violet"
 };
 
-const char* MinorColorNames[] = {
+static const char* MinorColorNames[] = {
     "Blue", "Orange", "Green", "Brown", "Slate"
 };
 
 
-const int numberOfMajorColors =
+static const int numberOfMajorColors =
     sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-const int numberOfMinorColors =
+static const int numberOfMinorColors =
     sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-const int MAX_COLORPAIR_NAME_CHARS = 16;
+static const int MAX_COLORPAIR_NAME_CHARS = 16;
 
 typedef struct {
     enum MajorColor majorColor;
